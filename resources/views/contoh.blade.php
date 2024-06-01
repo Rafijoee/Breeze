667ecb1b774db37fc8205566c55b81613bbcb66a
Rafijoee <rafijoe44@gmail.com>
2024-06-02 04:41:52 +0700

    fix landing page

---
+++ resources/views/contoh.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit sit eaque molestias perspiciatis cumque aperiam asperiores quas, officia et beatae numquam, necessitatibus tenetur libero fugiat placeat deleniti laudantium saepe qui accusamus dignissimos excepturi unde ipsa. Maiores dolorem eaque, aliquam voluptatum, reiciendis rerum ex totam molestiae quae alias labore ab iure. Corporis, quasi tempore quibusdam, nesciunt autem a voluptate perferendis inventore, amet voluptatibus deleniti necessitatibus? Quia consequatur, corporis, excepturi in magni similique voluptates dolor quos sed nam sit. Nemo eligendi facilis asperiores sit, sed tempora eaque blanditiis laudantium enim explicabo excepturi reprehenderit distinctio odit deserunt ea inventore, unde, impedit qui totam. Quia omnis ipsum quasi nobis ratione fuga vel iure asperiores nam voluptates repellendus sunt mollitia dicta, odit veniam magni! Enim asperiores, explicabo autem voluptas a doloremque possimus quas cupiditate, quam quaerat sequi nemo, earum architecto ex delectus in tempora ducimus. Atque est tenetur sint quia nisi voluptatem facere omnis iusto, animi maxime consequuntur error veniam consectetur ut vitae voluptate earum at vel ullam unde cum labore nihil cumque. Accusantium natus sapiente excepturi numquam esse laborum quia quod iusto ipsa animi dolorum temporibus sequi, vero veniam eius odio, dolorem ex? Tenetur enim dolorum aut, tempore sequi ad consequuntur sint provident eligendi?
+</body>
+</html>
